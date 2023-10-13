a23e1c5512ef5bb51d878257157b4f8c5ee41ac1
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-10-13 18:05:08 +0500

    swfgd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
+import "./App.css";
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
-      <Footer/>
-    
+      <Header />
+      <About />
+      <Footer />
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Header.scss'
-
+import React from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
 function Header() {
   return (
-    <div>
-      
+    <div className="Header">
+      <div className="container">
+        <div className="header__inner">
+          <nav className="nav">
+            <div className="logo">
+              <Link to={"/"}>
+                <img src={logo}alt="" />
+                <h1>Alimov</h1>
+              </Link>
+            </div>
+            <ul className="header__list">
+              <li>About me</li>
+              <li>Services</li>
+              <li>Contact me</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
